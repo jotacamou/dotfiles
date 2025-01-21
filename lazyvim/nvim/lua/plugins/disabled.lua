fd77efef9b91c1ae4cddc6fded250d304d5d9e75
JR Camou <jrcamou@herbalife.com>
2025-01-21 15:43:28 -0800

    feat: minor config changes

---
+++ lazyvim/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,4 @@
+--- Disabled plugins
+return {
+  --- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+}
---
+++ lazyvim/nvim/lua/plugins/mini-files.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.files",
+  opts = {
+    windows = {
+      width_preview = 80,
+    },
+    options = {
+      use_as_default_explorer = true,
+    },
+  },
+}
