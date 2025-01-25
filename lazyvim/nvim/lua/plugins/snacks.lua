70087aa84b58c639b928b0382181e95217d75cb7
JR Camou <jr.camou@stash.com>
2025-01-24 23:26:26 -0800

    Update dashboard banner

--- lazyvim/nvim/lua/plugins/snacks.lua
+++ lazyvim/nvim/lua/plugins/snacks.lua
@@ -1,9 +1,22 @@
 return {
-  opts = function(_, opts)
-    table.insert(
-      opts.dashboard.preset.keys,
-      7,
-      { icon = "S", key = "S", desc = "Select Session", action = require("persistence").select }
-    )
-  end,
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
+          end,
+          header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+ ]],
+        },
+      },
+    },
+  },
 }
